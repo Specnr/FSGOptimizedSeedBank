c3f3c1fca4eca9be688d03f34ef4c4fa8b205491
Specnr <specnrr@gmail.com>
2021-06-01 02:27:52 -0400

    added findSeed.sh

---
+++ findSeed.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import requests
+import json
+from multiprocessing import Process
+
+
+def display_seed(verif_data, seed):
+    if (seed == ""):
+        print("Seed Timed Out\n")
+    else:
+        print(f"Seed Found({verif_data['iso']}): {seed}")
+        print(f"Temp Token: {verif_data}\n")
+
+
+def run_seed(filter):
+    resp = requests.get(f"http://fsg.gel.webfactional.com?filter={filter}")
+    res_json = resp.json()
+    sseed = res_json.get("struct")
+    sclass = res_json.get("class")
+    randbiome = res_json.get("randbiome")
+    pref = res_json.get("pref")  # village and/or shipwreck preference
+    cmd = f'./bh {sseed} {sclass} {randbiome} {pref}'
+    seed = os.popen(cmd).read().strip()
+    display_seed(res_json, seed)
+
+
+def start_run(max_processes):
+    with open('filter.json') as filter_json:
+        filter = json.load(filter_json)["filter"]
+    processes = []
+    for i in range(max_processes):
+        processes.append(Process(target=run_seed, args=(filter,)))
+        processes[-1].start()
+    i = 0
+    while True:
+        for j in range(len(processes)):
+            if not processes[j].is_alive():
+                for k in range(len(processes)):
+                    processes[k].kill()
+                return
+        i = (i + 1) % max_processes
+
+
+if __name__ == '__main__':
+    PROCESSES_COUNT = int(sys.argv[1]) if len(sys.argv) >= 2 else 4
+    start_run(PROCESSES_COUNT)
